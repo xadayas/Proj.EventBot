a8d139451dce7c3a363fcb8bbc338e92f3ef9986
Adam Yassin <xadayas@gmail.com>
2016-08-16 13:58:14 +0200

    Init

---
+++ EventBot.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventBot.Web.MvcApplication" Language="C#" %>
---
+++ EventBot.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventBot.Web.Startup))]
+namespace EventBot.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
