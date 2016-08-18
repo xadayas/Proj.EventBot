41e654c340a59d651c75090c9812ac0c97c26821
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-08-18 10:03:57 +0200

    Added Search event test.

--- EventBot.Entities.Service.Test/ServiceTests.cs
+++ EventBot.Entities.Service.Test/ServiceTests.cs
@@ -46,6 +46,39 @@ namespace EventBot.Entities.Service.Test
             });
             Assert.That(firstEvent.Id==testId);
         }
+
+        [Test]
+        public void CanFindEventBasedOnTitleDescriptionLocation()
+        {
+            Console.WriteLine("Searching for events targeting title description and location, then adding event and redoing the search.");
+            Console.WriteLine("Each search phrase should increase by 1. Found ( before / after ) added test event.");
+
+            var foundTitleCount = _service.SearchEvents("banan").Count;
+            var foundDescriptionCount = _service.SearchEvents("MiniGolf").Count;
+            var foundLocationCount = _service.SearchEvents("Umeå").Count;
+
+            _service.CreateOrUpdateEvent(
+                new EventModel
+                {
+                    Title = "Banan ätar tävling ",
+                    Description = "Vi träffas och spelar minigolf",
+                    MeetingPlace = "Umeå",
+                    StartDate = DateTime.Now,
+                    EndDate = DateTime.Now
+                });
+            var foundTitleCount2 = _service.SearchEvents("banan").Count;
+            var foundDescriptionCount2 = _service.SearchEvents("MiniGolf").Count;
+            var foundLocationCount2 = _service.SearchEvents("Umeå").Count;
+
+            Assert.That(foundTitleCount2==foundTitleCount+1);
+            Assert.That(foundDescriptionCount2 == foundDescriptionCount + 1);
+            Assert.That(foundLocationCount2 == foundLocationCount + 1);
+
+            
+            Console.WriteLine($"Title : {foundTitleCount} / {foundTitleCount2} .");
+            Console.WriteLine($"Description : {foundTitleCount} / {foundTitleCount2} .");
+            Console.WriteLine($"Location : {foundTitleCount} / {foundTitleCount2} .");
+        }
     }
 
 }
