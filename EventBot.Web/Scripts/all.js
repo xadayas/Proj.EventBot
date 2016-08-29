2a1c40510a2e685cbf7cf242c5b2b8a7b649155d
Marcus Lönneborg <marcuslonneborg@gmail.com>
2016-08-29 13:14:07 +0200

    fixed

--- EventBot.Web/Scripts/all.js
+++ EventBot.Web/Scripts/all.js
@@ -1,94 +1,114 @@
-﻿$(document).ready(function () {
-    $.getJSON("/api/notifications/getNewNotifications", function (notifications) {
-        if (notifications.length == 0)
-            return;
+﻿//$(document).ready(function () {
+//    $.getJSON("/api/notifications/getNewNotifications", function (notifications) {
+//        if (notifications.length == 0)
+//            return;
 
 
-        $(".js-notification-count")
-            .text(notifications.length)
-            .removeClass("hide")
-            .addClass("animated bounceInDown");
+//        $(".js-notification-count")
+//            .text(notifications.length)
+//            .removeClass("hide")
+//            .addClass("animated bounceInDown");
 
-        $(".notifications").popover({
-            html: true,
-            title: "Notifikationer",
-            content: function () {
-                var compiled = _.template($("#notifications-template").html());
-                return compiled({ notifications: notifications });
-            },
-            placement: "bottom",
-            template: '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
-        }).on("shown.bs.popover", function () {
-            $.post("/api/notifications/markAllAsRead")
-                .done(function () {
-                    $(".js-notification-count")
-                        .text("")
-                        .addClass("hide");
-                });
-        });
-    });
-});
-
-//var MyNotifications;
-
-//$(document).ready(function () {
-//            $.getJSON("/api/Notifications/GetNewNotifications/", function (notifications) {
-//                if (notifications.length == 0)
-//                    return;
-//                MyNotifications = notifications;
-//                console.log(notifications);
-//                $("#notificationsbadge")
-//                    .text(notifications.length)
-//                    .removeClass("hide")
-//                    .addClass("animated bounceInDown");
-
-//                $(".notifications").popover({
-//                html: true,
-//                title: "Notifikationer",
-//                content: function () {
-//                        var compiled = _.template($("#notifications-template").html());
-//                        return compiled({ notifications: notifications });
+//        $(".notifications").popover({
+//            html: true,
+//            title: "Notifikationer",
+//            content: function () {
+//                var compiled = _.template($("#notifications-template").html());
+//                return compiled({ notifications: notifications });
 //            },
-//                placement: "bottom",
-//                template: '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><div id="notcontent"></div></div>'
-
-//            }).on("shown.bs.popover", function() {
-//            //    $.post("/api/Notifications/MarkAllAsRead")
-//            //            .done(function () {
-//            //                $(".js-notifications-count")
-//            //                    .text("")
-//            //                    .addClass("hide");
+//            placement: "bottom",
+//            template: '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
+//        }).on("shown.bs.popover", function () {
+//            //$.post("/api/notifications/markAllAsRead")
+//            //    .done(function () {
+//                    $(".js-notification-count")
+//                        .text("")
+//                        .addClass("hide");
 //                //});
-//                    UpdateNotifications();
-//                });
-//            });
+//        });
+//    });
 //});
 
-//function UpdateNotifications() {
-//    $.each(MyNotifications, function (i, item) {
-//        $('#notcontent').append('<div class="row"><button type="button" class="btn btn-default btn-block" onClick="OnNotificationClicked(' + item.id+","+item.eventId + ')">' + item.eventName +" " +NotificationTypeToString(item.eventType)+ "</button> </div>");
-//    });
-//}
+var MyNotifications;
 
-//function OnNotificationClicked(notId, eventId) {
-//    console.log(notId + " " + eventId);
-//    $.post("/api/Notifications/MarkSingleAsRead/"+notId)
-//                .done(function () {
-//                    window.location = "/event/details/" + eventId;
-//                });
-//}
-//function NotificationTypeToString(ntype) {
+$(document).ready(function () {
+            $.getJSON("/api/Notifications/GetNewNotifications/", function (notifications) {
+                if (notifications.length == 0)
+                    return;
+                MyNotifications = notifications;
+                console.log(notifications);
+                $("#notificationsbadge")
+                    .text(notifications.length)
+                    .removeClass("hide")
+                    .addClass("animated bounceInDown");
+
+                $(".notifications").popover({
+                html: true,
+                title: "Notifikationer",
+                content: function () {
+                    console.log("snopp");
+                        var compiled = _.template($("#notifications-template").html());
+                        return compiled({ notifications: notifications });
+            },
+                placement: "bottom",
+                template: '<div class="popover popover-notifications" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div><div id="notcontent"></div></div>'
+
+            }).on("shown.bs.popover", function() {
+            //    $.post("/api/Notifications/MarkAllAsRead")
+            //            .done(function () {
+            //                $(".js-notifications-count")
+            //                    .text("")
+            //                    .addClass("hide");
+                //});
+                    UpdateNotifications();
+                });
+            });
+});
+
+function UpdateNotifications() {
+    $.each(MyNotifications, function (i, item) {
+        if (item.eventType==1) {
+            $('#notcontent').append('<li onClick="OnNotificationClicked(' +item.id +','+ item.eventId+');">Planerade aktiviteten <span class="highlight">'+ item.eventName +'</span> som var planerad '+ moment(item.dateTime).format("D MMM HH:mm")+ ' är tyvärr inställd.</li>');
+        }
+        else if (item.eventType == 2) {
+            var changes = [],
+            originalValues = [],
+            newValues = [];
+
+            if (item.originalStartDate != item.startDate) {
+                changes.push('datum/tid');
+                originalValues.push(moment(item.originalStartDate).format("D MMM HH:mm"));
+                newValues.push(moment(item.startDate).format("D MMM HH:mm"));
+
+                $('#notcontent').append('<li onClick="OnNotificationClicked(' + item.id + ',' + item.eventId + ');"><span class="highlight">' + item.eventName + '</span> har ändrat ' + changes.join(" and ") + ' från ' + originalValues.join("/") + ' till ' + newValues.join("/") + '</li>');
+            } else {
+                $('#notcontent').append('<li onClick="OnNotificationClicked(' + item.id + ',' + item.eventId + ');"><span class="highlight">' + item.eventName + '</span> har uppdaterat aktiviteten som är planerad ' + moment(item.startDate).format("D MMM HH:mm") + '.</li>');
+            }
+         
+        }   
+       
+    });
+}
+
+function OnNotificationClicked(notId, eventId) {
+    console.log(notId + " " + eventId);
+    $.post("/api/Notifications/MarkSingleAsRead/"+notId)
+                .done(function () {
+                    window.location = "/event/details/" + eventId;
+                });
+}
+function NotificationTypeToString(ntype) {
     
-//    if (ntype === 1)
-//        return "Cancelled";
-//    else if (ntype === 2)
-//        return "Updated";
-//    else if (ntype === 3)
-//        return "Created";
-//    else {
-//        return "wat";
-//    }
-//}
+    if (ntype === 1)
+        return "Cancelled";
+    else if (ntype === 2)
+        return "Updated";
+    else if (ntype === 3)
+        return "Created";
+    else {
+        return "wat";
+    }
+}
 //$.getScript('https://www.google.com/jsapi', function () {
 //    google.load('maps', '3', {
 //        callback: function () {
