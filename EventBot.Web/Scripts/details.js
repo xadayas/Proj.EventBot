4cfec7a2a78789a9b5aff74a8016d4aa13be3fc2
Apparaten <bratt.anders@gmail.com>
2016-08-31 22:10:53 +0200

    Added "alert" to details, and changed Join/leave button to default button - Gå med/Lämna

--- EventBot.Web/Scripts/details.js
+++ EventBot.Web/Scripts/details.js
@@ -1,27 +1,35 @@
 ﻿
 var IsAttending = false;
 var eventId = 0;
-$(document).ready(function () {
+//$(document).ready(function () {
+//   // OnReady();
+//});
+function OnReady() {
     eventId = $("#hdnVal").val();
+    buildButton();
+}
+
+function buildButton() {
     checkIsAttending(eventId);
 
     var jl = IsAttending ? "Leave" : "Join";
-    var joinorleave = '<input type="submit" id="bt" value=' + jl + "></input>";
+    $("#attend").empty();
+    var joinorleave = '<input type="submit" class="btn btn-default" id="bt" value=' + jl + "></input>";
     $(joinorleave).appendTo('#attend');
     $('#bt').click(function (event) {
         event.preventDefault();
         JoinOrLeave();
         return false;
     });
-});
+    $("#alertbox").empty();
+}
 function checkIsAttending(eventId) {
     if (eventId == null)return;
     $.getJSON('/participants/isattending/' + eventId, function (attendStatus) {
         IsAttending = attendStatus.Attending;
-        var jl = IsAttending ? "Leave" : "Join";
+        var jl = IsAttending ? "Gå med" : "Lämna";
         $("#bt").val(jl);
     });
-   
 }
 
 function ToFunctionName() {
@@ -33,11 +41,25 @@ function JoinOrLeave() {
         contentType: "application/json; charset=utf-8",
         url: '/Participants/' + ToFunctionName()+'/'+eventId,
         success: function (data) {
-            checkIsAttending(eventId);
+            buildButton();
+            //checkIsAttending(eventId);
+            SetAlert();
+            
         },
         fail: function (data) {
             console.log("NO");
             checkIsAttending(eventId);
         }
     });
+}
+function SetAlert() {
+    $("#alertbox").empty();
+    var joined = '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Välkommen!</strong> Kom ihåg att inte glömma något du behöver ta med dig!</div>';
+    var leaved = '<div class="alert alert-warning alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Glöm inte bort!</strong> Att det är bara att anmäla sig igen om du ändrar dig! :)</div>';
+    if (!IsAttending)
+        $("#alertbox").append(joined);
+    else {
+        $("#alertbox").append(leaved);
+    }
+
 }
