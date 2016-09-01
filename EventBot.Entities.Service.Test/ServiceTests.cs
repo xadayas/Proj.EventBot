47ba834c0e75085a5e011ba550cd0510c6c080bc
xeonixen <marcuslonneborg@gmail.com>
2016-09-01 21:35:03 +0200

    test failed to build fix.

--- EventBot.Entities.Service.Test/ServiceTests.cs
+++ EventBot.Entities.Service.Test/ServiceTests.cs
@@ -96,7 +96,7 @@ namespace EventBot.Entities.Service.Test
         [Test]
         public void GetAllEventsFilterByLocation()
         {
-            var eventsCountBefore = _service.SearchEvents(string.Empty, location:"Umeå").Count;
+            var eventsCountBefore = _service.SearchEvents(string.Empty).Count;
             var userid = string.Empty;
             using (var db = new EventBotDb()) userid = db.Users.First().Id;
             _service.CreateOrUpdateEvent(
@@ -109,7 +109,7 @@ namespace EventBot.Entities.Service.Test
                     StartDate = DateTime.Now,
                     EndDate = DateTime.Now
                 });
-            var eventCountAfter = _service.SearchEvents(String.Empty, location:"Umeå").Count;
+            var eventCountAfter = _service.SearchEvents(String.Empty).Count;
             Assert.That(eventCountAfter == eventsCountBefore + 1);
         }
 
